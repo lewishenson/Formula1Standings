2829935c518c40cadc32c907c7389096dc8362cd
Lewis Henson <lewishenson@users.noreply.github.com>
2014-10-30 10:04:51 +0000

    Show dynamic list of drivers

--- app/app.js
+++ app/app.js
@@ -1,32 +1,27 @@
 'use strict';
 
 angular.module('F1FeederApp', [
-  'F1FeederApp.controllers'
+  'F1FeederApp.controllers',
+  'F1FeederApp.services'
 ]);
 
-angular.module('F1FeederApp.controllers', []).controller('driversController', function($scope) {
-    $scope.driversList = [
-      {
-          Driver: {
-              givenName: 'Sebastian',
-              familyName: 'Vettel'
-          },
-          points: 322,
-          nationality: "German",
-          Constructors: [
-              {name: "Red Bull"}
-          ]
-      },
-      {
-          Driver: {
-          givenName: 'Fernando',
-              familyName: 'Alonso'
-          },
-          points: 207,
-          nationality: "Spanish",
-          Constructors: [
-              {name: "Ferrari"}
-          ]
-      }
-    ];
+angular.module('F1FeederApp.controllers', []).controller('driversController', function($scope, ergastAPIservice) {
+    $scope.driversList = [];
+
+    ergastAPIservice.getDrivers().success(function (response) {
+        $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
 });
+
+angular.module('F1FeederApp.services', []).  factory('ergastAPIservice', function($http) {
+    var ergastAPI = {};
+
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2014/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    return ergastAPI;
+  });
